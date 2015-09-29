962517f4136c64f781fe8d4a1857974eaad96501
Sean McGregor <smcgregor08@cmc.edu>
2015-09-29 10:04:35 -0700

    added ability to select multiple domains from the domain_bridge

--- serve.sh
+++ serve.sh
@@ -3,4 +3,5 @@ echo "This script starts a simple Python server that will let you open MDPvis lo
 echo "Once the server starts you can visit http://localhost:8000/index.html in your browser."
 echo "to quit the server you should press ctrl-c."
 echo ""
+echo "You should not use this server to start an MDP domain since this will only serve the visualization."
 python -m SimpleHTTPServer
--- tornado_server.py
+++ tornado_server.py
@@ -4,5 +4,9 @@ from tornado.ioloop import IOLoop
 from flask_server import app
 
 http_server = HTTPServer(WSGIContainer(app))
+# There is an optional parameter for selecting between multiple domains in the
+# domain bridge
+if len(sys.argv) > 2:
+    domain = sys.argv[2]
 http_server.listen(5000)
 IOLoop.instance().start()

f9078a56b5102697feb6d6102d796461c062529a
Sean McGregor <smcgregor@seanbmcgregor.com>
2015-09-30 15:34:20 -0700

    added import sys to tornado server

--- tornado_server.py
+++ tornado_server.py
@@ -2,6 +2,7 @@ from tornado.wsgi import WSGIContainer
 from tornado.httpserver import HTTPServer
 from tornado.ioloop import IOLoop
 from flask_server import app
+import sys
 
 http_server = HTTPServer(WSGIContainer(app))
 # There is an optional parameter for selecting between multiple domains in the
